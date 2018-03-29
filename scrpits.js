cd86e32ef6af4d1331c751d555b567aac5167949
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-03-29 18:45:45 +0200

    correct

--- scrpits.js
+++ scrpits.js
@@ -10,16 +10,16 @@ Phone.prototype.printInfo = function() {
 	console.log("The phone brand is" + this.brand + ", color is " + this.color + "and the price is" + this.price + "." + "Display is" + this.display + " inch.");
 }
 
-var iPhone6S = new Phone("Apple", 2250, "silver", 4,7);
+var iPhone6S = new Phone("Apple", 2250, "silver", "4,7");
 
 iPhone6S.printInfo();
 
 
-var GalaxyS6 = new Phone("Samsung", 1000, "white", 5,1);
+var GalaxyS6 = new Phone("Samsung", 1000, "white", "5,1");
 
 GalaxyS6.printInfo();
 
 
-var OnePlusOne = new Phone("HTC", 1500, "black", 5,5);
+var OnePlusOne = new Phone("HTC", 1500, "black", "5,5");
 
 OnePlusOne.printInfo();
